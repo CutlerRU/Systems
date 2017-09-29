f3f8878ca2101e159554da09bcadee5c3b168399
tusharma97 <tusharma97@gmail.com>
2017-09-29 16:38:27 -0400

    Initial Commit

---
+++ Systems/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Systems
+//
+//  Created by Tushar Sharma on 9/29/17.
+//  Copyright © 2017 Tushar Sharma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
